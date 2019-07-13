badfe42ad9094c518e3ad99c6c9409a024cd3d88
Esa Prakasa <esa.prakasa@gmail.com>
2019-07-14 05:48:38 +0700

    Create applyfilter.py
    This function is used to read an image. A square average filter is then applied to the image.

---
+++ applyfilter.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+
+# read the image which is stored in the same folder with the code
+img = cv2.imread('hello.jpg')
+
+# fSz is the filter size. The size should be an ood number.
+fSz = 21
+
+# creating the filter
+filt = np.ones((fSz,fSz),np.float32)/(fSz*fSz)
+dst = cv2.filter2D(img,-1, filt)
+ 
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
+plt.xticks([]), plt.yticks([])
+plt.show()
