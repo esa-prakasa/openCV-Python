6c3ad200f14ab0f77b11b9fb75ed7eb1caeace3b
Esa Prakasa <esa.prakasa@gmail.com>
2019-07-20 12:06:27 +0700

    Create getcam.py
    Get video from connected webcam.

---
+++ getcam.py
@@ -0,0 +1,26 @@
+#reference: https://www.youtube.com/watch?v=1XTqE7LFQjI
+
+import cv2, time
+
+video = cv2.VideoCapture(0)
+a = 0
+while True:
+	a = a + 1
+
+	check, frame = video.read()
+
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	#cv2.imshow("Capturing",frame)
+	cv2.imshow("Capturing",frame)
+
+	#cv2.waitKey(0)
+	key = cv2.waitKey(1)
+
+	if key == ord('q'):
+		break
+	print("  "+str(a))
+
+
+video.release()
+cv2.destroyallwindows()
