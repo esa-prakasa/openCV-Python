1e6e2849aa2f8abfb2a25f89b5e8970513b59e32
Esa Prakasa <esa.prakasa@gmail.com>
2019-10-22 00:58:46 +0200

    Put note on the py file

--- readimg.py
+++ readimg.py
@@ -1,3 +1,6 @@
+# This code is used to read a file image. The image is stored as img variable. The image is then resized according to the ratio. 
+# The ratio should be a positive number. If the ratio value less than 1, then the resized image will be smaller than its original size.
+
 import numpy as np
 import cv2
 img = cv2.imread("fileName.jpg",1)
